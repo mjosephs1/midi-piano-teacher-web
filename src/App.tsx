181b28fa7e2ef6a6d8cef5cc81b9146585235c3b
Matthew Josephs <mattjosephs2@gmail.com>
2026-05-17 21:24:38 -0400

    Convert boilerplate to use typescsript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import type { FC } from 'react';
 
-function App() {
+const App: FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,6 +21,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
